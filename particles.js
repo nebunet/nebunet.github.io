a0772ee5923452728f18a33329ea84382d56f574
nebunet <109931844+nebunet@users.noreply.github.com>
2023-01-02 18:28:59 -0500

    Update particles.js

--- particles.js
+++ particles.js
@@ -5,11 +5,11 @@ let pj_div = document.createElement("div");
 pj_div.id = "particles-js";
 pj_div.style =
   "position: absolute;width: 100vw;height:100vh;;overflow-y: hidden;";
+document.body.appendChild(pj_div);
 
 script1.src =
   "https://cdn.jsdelivr.net/gh/nebunet/nebunet.github.io/pj_particles.js";
 script2.src = "https://cdn.jsdelivr.net/gh/nebunet/nebunet.github.io/pj_app.js";
 
-document.body.appendChild(pj_div);
 document.head.appendChild(script1);
 document.head.appendChild(script2);
